7996a2279554f1dd8bac5fe796c71859c2b0d881
ldenso34046 <48933541+ldenso34046@users.noreply.github.com>
2021-04-09 18:18:40 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -21,7 +21,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
 },
 
   onEachFeature: function(feature, layer) {
-  layer.bindPopup(feature.properties.headline + '</br> </br>Alert Type: ' + feature.properties.severity);
+  layer.bindPopup(feature.properties.headline + '</br> </br>Severity Type: ' + feature.properties.severity + '</br>Certainty: ' + feature.properties.certainty + '</br>Urgency: ' + feature.properties.urgency);
 }
 
 }).addTo(map);
